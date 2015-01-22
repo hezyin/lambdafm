c34e75446c560034b32e1718e1fa5e5b58f2365f
UltimateBeaver <tlhly@hotmail.com>
2015-01-22 12:52:38 +0800

    add correct ptuner 0121

---
+++ ptuner_0121c.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+ps = []
+1.upto(8) do |i|
+  1.upto(5) do |j|
+    ps << [i * 0.001 + 0.002, j * 0.00002]
+  end
+end
+
+total = ps.size
+start = Time.now
+f = open("ptuner_ruby_log", "w+")
+
+ps.each_with_index do |p, i|
+  f.puts "#{i+1}/#{total} k=16 e2=#{p[0]} l2=#{p[1]} #{(Time.now - start)/60}mins" 
+  `./lambdafm -k 16 -t 8 -s 16 -e2 #{p[0]} -e3 0.003 -l2 #{p[1]} -g k_16_e2_#{p[0]}_l2_#{p[1]} train.libfm test.libfm`
+end
+
+f.close
+
+
